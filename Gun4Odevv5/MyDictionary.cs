b365db9b4f4ffabc70849a1ef70119021971b35e
Beyza Baltacı <beyza_baltacii@hotmail.com>
2021-02-23 21:24:03 +0300

    Add project files.

---
+++ Gun4Odevv5/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gun4Odevv5
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] number;
+        T2[] name;
+
+        public MyDictionary()
+        {
+            number = new T1[0];
+            name = new T2[0];
+        }
+        public void Add(T1 numbers, T2 names)
+        {
+            T1[] tempNumber = number;
+            T2[] tempName = name;
+
+            number = new T1[number.Length + 1];
+            name = new T2[name.Length + 1];
+
+            for (int i = 0; i < tempNumber.Length; i++)
+            {
+                number[i] = tempNumber[i];
+
+            }
+            number[number.Length - 1] = numbers;
+
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+            name[name.Length - 1] = names;
+        }
+        public void ToList()
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i] + ": " + name[i]);
+            }
+
+
+
+        }
+    }
+}
---
+++ Gun4Odevv5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gun4Odevv5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(1234, "Beyza Baltacı");
+            students.Add(1263, "Belgin Kaya");
+            students.Add(1458, "Derya Malçok");
+            students.Add(1254, "Nurbanu Kara");
+
+            students.ToList();
+        }
+    }
+}
